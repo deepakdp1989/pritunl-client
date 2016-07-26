1dccc63e885fab484e191398e7a4e4018c9e4aff
Zachary Huff <zach.huff.386@gmail.com>
2016-07-26 03:51:34 -0400

    Create release on gitlab

--- tools/builder.py
+++ tools/builder.py
@@ -106,6 +106,7 @@ with open(BUILD_KEYS_PATH, 'r') as build_keys_file:
     build_keys = json.loads(build_keys_file.read().strip())
     github_owner = build_keys['github_owner']
     github_token = build_keys['github_token']
+    gitlab_token = build_keys['gitlab_token']
     mirror_url = build_keys['mirror_url']
     test_mirror_url = build_keys['test_mirror_url']
 
@@ -257,7 +258,7 @@ elif cmd == 'set-version':
     if not is_snapshot:
         subprocess.check_call(['git', 'branch', new_version])
         subprocess.check_call(['git', 'push', '-u', 'origin', new_version])
-    time.sleep(8)
+    time.sleep(6)
 
 
     # Create release
@@ -284,6 +285,28 @@ elif cmd == 'set-version':
 
     subprocess.check_call(['git', 'pull'])
     subprocess.check_call(['git', 'push', '--tags'])
+    time.sleep(6)
+
+
+    # Create gitlab release
+    response = requests.post(
+        'https://git.pritunl.com/api/v3/projects' + \
+        '/%s%%2F%s/repository/tags/%s/release' % (
+            github_owner, pkg_name, new_version),
+        headers={
+            'Private-Token': gitlab_token,
+            'Content-type': 'application/json',
+        },
+        data=json.dumps({
+            'tag_name': new_version,
+            'description': release_body,
+        }),
+    )
+
+    if response.status_code != 201:
+        print 'Failed to create release on gitlab'
+        print response.json()
+        sys.exit(1)
 
 
 elif cmd == 'build':
