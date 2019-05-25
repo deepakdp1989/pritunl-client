9d2b886988cc4dddaeb870eecd050c85ebe19b5f
Zachary Huff <zach.huff.386@gmail.com>
2019-05-24 21:41:38 -0400

    Remove token in profile data

--- pritunl_client/profile.py
+++ pritunl_client/profile.py
@@ -41,10 +41,6 @@ class Profile(object):
         self.org_id = None
         self.server_id = None
         self.password_mode = None
-        self.token = None
-        self.token_ttl = None
-        self.auth_token = None
-        self.auth_token_time = None
         self.sync_hash = None
         self.sync_token = None
         self.sync_secret = None
@@ -79,10 +75,6 @@ class Profile(object):
             'org_id': self.org_id,
             'server_id': self.server_id,
             'password_mode': self.password_mode,
-            'token': self.token,
-            'token_ttl': self.token_ttl,
-            'auth_token': self.auth_token,
-            'auth_token_time': self.auth_token_time,
             'sync_hash': self.sync_hash,
             'sync_token': self.sync_token,
             'sync_secret': self.sync_secret,
@@ -137,10 +129,6 @@ class Profile(object):
                     self.org_id = data.get('org_id')
                     self.server_id = data.get('server_id')
                     self.password_mode = data.get('password_mode')
-                    self.token = data.get('token')
-                    self.token_ttl = data.get('token_ttl')
-                    self.auth_token = data.get('auth_token')
-                    self.auth_token_time = data.get('auth_token_time')
                     self.sync_hash = data.get('sync_hash')
                     self.sync_token = data.get('sync_token')
                     self.sync_secret = data.get('sync_secret')
@@ -208,9 +196,6 @@ class Profile(object):
         self.server_id = conf_data.get('server_id', self.server_id)
         self.password_mode = conf_data.get(
             'password_mode', self.password_mode)
-        self.token = conf_data.get('token', self.token)
-        self.token_ttl = conf_data.get(
-            'token_ttl', self.token_ttl)
         self.sync_hash = conf_data.get('sync_hash', self.sync_hash)
         self.sync_token = conf_data.get('sync_token', self.sync_token)
         self.sync_secret = conf_data.get('sync_secret', self.sync_secret)
